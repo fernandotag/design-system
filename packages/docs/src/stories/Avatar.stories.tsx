7c6aa436dd7a97fabc98e0b7c833171a2387430e
Fernando Tagliaferro <4048111+fernandotag@users.noreply.github.com>
2023-09-05 16:52:11 -0300

    feat: adjust component controls

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/fernandotag.png',
     alt: 'Fernando Tagliaferro',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
